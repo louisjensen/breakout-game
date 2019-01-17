1179c40ae0eef93f881020a6a4161646858b62cc
Louis Jensen <lbj7@duke.edu>
2019-01-17 10:01:43 -0500

    starting

---
+++ src/game/myGame.java
@@ -0,0 +1,84 @@
+package game;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+
+/**
+ * A basic example JavaFX program for the first lab.
+ *
+ * @author Robert C. Duvall
+ */
+public class myGame extends Application {
+    public static final String TITLE = "Breakout Game";
+    public static final int SIZE = 400;
+    public static final int FRAMES_PER_SECOND = 60;
+    public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+    public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+    public static final Paint BACKGROUND = Color.AZURE;
+    public static final Paint HIGHLIGHT = Color.OLIVEDRAB.;
+    public static final String BALL_IMAGE = "ball.gif";
+    public static final int BALL_SPEED = 60;
+    public static final String PADDLE_IMAGE = "paddle.gif";
+
+    private Scene myScene;
+    private ImageView myBall;
+    private ImageView myPaddle;
+
+    /**
+     * Initialize what will be displayed and how it will be updated.
+     */
+    @Override
+    public void start (Stage stage) {
+        // attach scene to the stage and display it
+        myScene = setupGame(SIZE, SIZE, BACKGROUND);
+        stage.setScene(myScene);
+        stage.setTitle(TITLE);
+        stage.show();
+        // attach "game loop" to timeline to play it
+        var frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step(SECOND_DELAY));
+        var animation = new Timeline();
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.getKeyFrames().add(frame);
+        animation.play();
+    }
+
+    // Create the game's "scene": what shapes will be in the game and their starting properties
+    private Scene setupGame (int width, int height, Paint background) {
+        // create one top level collection to organize the things in the scene
+        var root = new Group();
+        // create a place to see the shapes
+        var scene = new Scene(root, width, height, background);
+        // make some shapes and set their properties
+        var image = new Image(this.getClass().getClassLoader().getResourceAsStream(BOUNCER_IMAGE));
+        myBall = new ImageView(image);
+        // x and y represent the top left corner, so center it
+        myBall.setX(width / 2 - myBall.getBoundsInLocal().getWidth() / 2);
+        myBall.setY(height / 2 - myBall.getBoundsInLocal().getHeight() / 2);
+        myPaddle.setX(width / 2 - myPaddle.getBoundsInLocal().getWidth() / 2 - 50);
+        myPaddle.setY(height / 2 - myPaddle.getBoundsInLocal().getHeight() / 2 - 50);
+        // order added to the group is the order in which they are drawn
+        root.getChildren().add(myBall);
+        root.getChildren().add(myPaddle);
+        // respond to input
+        scene.setOnKeyPressed(e -> handleKeyInput(e.getCode()));
+        scene.setOnMouseClicked(e -> handleMouseInput(e.getX(), e.getY()));
+        return scene;
+    }
+
+
+
+
+}
