2cfca9abb8654d8cd10c467cd963053fb8ce845a
Louis Jensen <lbj7@duke.edu>
2019-01-18 15:20:16 -0500

    Ball bouncing of walls and paddle

--- src/game/Ball.java
+++ src/game/Ball.java
@@ -16,54 +16,54 @@ import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-public class Paddle {
-    private Rectangle paddle;
-    private int paddleWidth = 80;
-    private int paddleHeight = 10;
-    private int paddleSpeed = 10;
-    private Paint paddleColor = Color.CRIMSON;
+public class Ball {
+    private Circle ball;
+    private int ballRadius = 5;
+    private int ballSpeed = 60;
+    private Paint ballColor = Color.GREEN;
     private static final int FRAMES_PER_SECOND = 60;
     private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
     private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+    private int ballXSpeed = 50;
+    private int ballYSpeed = 50;
 
-    public Paddle(Scene currScene){
-        paddle = new Rectangle(currScene.getWidth() / 2 - paddleWidth / 2,
-                               5*currScene.getHeight() / 6 - paddleHeight / 2,
-                                paddleWidth, paddleHeight);
-        paddle.setFill(paddleColor);
-        var frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> paddleStep(SECOND_DELAY));
+    public Ball(Scene currScene){
+        ball = new Circle(currScene.getWidth() / 2 - ballRadius / 2,
+                            5*currScene.getHeight() / 6 - ballRadius*2,
+                            ballRadius,
+                            ballColor);
+        var frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> ballStep(SECOND_DELAY));
         var animation = new Timeline();
         animation.setCycleCount(Timeline.INDEFINITE);
         animation.getKeyFrames().add(frame);
     }
 
-    private void paddleStep(double elapsedTime){
-      //  paddle.setX(paddle.getX());
-     //   paddle.setY(paddle.getY());
-
+    private void ballStep(double elapsedTime){
+        ball.setCenterX(ball.getCenterX() + ballXSpeed * elapsedTime);
+        ball.setCenterY(ball.getCenterY() + ballYSpeed * elapsedTime);
     }
 
-    public Rectangle getPaddle(){
-        return paddle;
-    }
-
-    public int getSpeed(){
-        return paddleSpeed;
+    public Circle getBall() {
+        return ball;
     }
 
     public double getX(){
-        return paddle.getX();
+        return ball.getCenterX();
     }
 
     public double getY(){
-        return paddle.getY();
+        return ball.getCenterY();
     }
 
     public void setX(double speed){
-        paddle.setX(speed);
+        ball.setCenterX(speed);
     }
 
     public void setY(double speed){
-        paddle.setY(speed);
+        ball.setCenterY(speed);
+    }
+
+    public int getBallRadius() {
+        return ballRadius;
     }
 }
--- src/game/GameDriver.java
+++ src/game/GameDriver.java
@@ -19,7 +19,7 @@ import javafx.event.EventHandler;
 import javafx.event.EventType;
 
 public class GameDriver {
-    private static final int SIZE = 400;
+    private static final int SIZE = 500;
     private static final int FRAMES_PER_SECOND = 60;
     private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
     private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
@@ -31,20 +31,31 @@ public class GameDriver {
     public static final int MOVER_SPEED = 5;
     private Scene myScene;
     private Paddle paddle;
+    private Ball ball;
+    private int ballXSpeed = (int)(Math.random() * 160 + 1) - 80;
+    private int ballYSpeed = -80;
 
     public void startGame(Stage stage){
         GAME_STAGE = stage;
         GAME_STAGE.setTitle(TITLE);
         GAME_STAGE.setTitle(TITLE);
         GAME_STAGE.show();
+        var frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step(SECOND_DELAY));
+        var animation = new Timeline();
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.getKeyFrames().add(frame);
+        animation.play();
     }
 
     public void playGame(){
         var root = new Group();
         Scene beginScene = new Scene(root, SIZE, SIZE, BACKGROUND);
+        myScene = beginScene;
         beginScene.setOnKeyPressed(e -> handleKeyInput(e.getCode()));
         paddle = new Paddle(beginScene);
+        ball = new Ball(beginScene);
         root.getChildren().add(paddle.getPaddle());
+        root.getChildren().add(ball.getBall());
         GAME_STAGE.setScene(beginScene);
     }
 
@@ -57,11 +68,28 @@ public class GameDriver {
     } */
 
     private void handleKeyInput (KeyCode code) {
-        if (code == KeyCode.RIGHT) {
+        if (code == KeyCode.RIGHT && paddle.getX() < myScene.getWidth()) {
             paddle.setX(paddle.getX() + paddle.getSpeed());
         }
         else if (code == KeyCode.LEFT) {
             paddle.setX(paddle.getX() - paddle.getSpeed());
+        }
+    }
+
+    private void step (double elapsedTime) {
+        // update attributes
+        ball.setX(ball.getX() + ballXSpeed * elapsedTime);
+        ball.setY(ball.getY() + ballYSpeed * elapsedTime);
+
+        if (ball.getX() > myScene.getWidth() - ball.getBallRadius() || ball.getX() < - ball.getBallRadius()) {
+            ballXSpeed*=-1;
+        }
+        if (ball.getY() < - ball.getBallRadius()) {
+            ballYSpeed*=-1;
+        }
+        var intersect = Shape.intersect(ball.getBall(), paddle.getPaddle());
+        if (intersect.getBoundsInLocal().getWidth() != -1) {
+            ballYSpeed*=-1;
         }
     }
 }
